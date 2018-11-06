dd64ad50fd9acbee1fc534de624ebd1ea32f81b0
aeperea <elizondoperea@gmail.com>
2018-11-06 13:25:04 -0600

    Adding basis structure for the job form header

--- src/views/job-form/JobFormComponent.jsx
+++ src/views/job-form/JobFormComponent.jsx
@@ -3,7 +3,13 @@ import './_JobForm.scss'
 import React from 'react'
 
 export const JobFormComponent = () => <div className="JobForm">
-  This will be the Job Form
+  <div className="JobForm--header">
+    <Icon number={1} />
+    <div className="JobForm--subheader">
+      <div className="JobForm--title">Job criteria</div>
+      <div className="JobForm--description">We will use the data we collect here to better target your desired audicience.</div>
+    </div>
+  </div>
 </div>
 
 export default JobFormComponent
