1d9629afbb459b5483e5f86d0579776e28dd51ad
Alejandro Perea <elizondoperea@gmail.com>
2018-11-06 04:53:10 -0600

    Adding the basis for redux, ui and the views

---
+++ src/ui/button/ButtonComponent.jsx
@@ -0,0 +1,9 @@
+import './_Button.scss'
+
+import React from 'react'
+
+export const ButtonComponent = () => <div className="Button">
+  This will be the Button
+</div>
+
+export default ButtonComponent 
---
+++ src/views/job-form/JobFormComponent.jsx
@@ -0,0 +1,9 @@
+import './_JobForm.scss'
+
+import React from 'react'
+
+export const JobFormComponent = () => <div className="JobForm">
+  This will be the Job Form
+</div>
+
+export default JobFormComponent
