24d5c58e467fcf09c89bdebc5fa90727b60c3029
aeperea <elizondoperea@gmail.com>
2018-11-06 20:59:01 -0600

    Adding rest of elements for the form

--- src/views/job-form/JobFormComponent.jsx
+++ src/views/job-form/JobFormComponent.jsx
@@ -1,15 +1,70 @@
 import './_JobForm.scss'
 
 import React from 'react'
+import Icon from '../../ui/icon'
+import Button from '../../ui/button'
+import Checkbox from '../../ui/checkbox'
 
-export const JobFormComponent = () => <div className="JobForm">
-  <div className="JobForm--header">
-    <Icon number={1} />
+const LEVELS = [
+  'Bachelor / Graduate',
+  'GCSE / A-Level / High School / GED',
+  'Master / Post-Graduate / PhD',
+  'Vocational / Diploma / Associates degree'
+];
+
+const JobFormHeader = () => <div className="JobForm--header">
+    <Icon type="number">1</Icon>
     <div className="JobForm--subheader">
-      <div className="JobForm--title">Job criteria</div>
+      <h2 className="JobForm--title">Job criteria</h2>
       <div className="JobForm--description">We will use the data we collect here to better target your desired audicience.</div>
     </div>
   </div>
+
+export const onSubmit = () => {
+  console.log("submitting")
+}
+
+export const JobFormComponent = () => <div className="JobForm">
+  <JobFormHeader />
+  <form>
+    <div className="JobForm--section">
+      <label className="JobForm--sectionText">A minimum No. years of experience</label>
+      <input
+        placeholder="i.e. 5+"
+        type="text"
+        value={''}
+      />
+    </div>
+
+    <div className="JobForm--section">
+      <label className="JobForm--sectionText">Level of education</label>
+      {LEVELS.map((level, index) =>
+          <Checkbox
+            key={`education-${index}`}
+            name={level}
+            checked={true}
+            onChange={() => {console.log('checked')}}>
+          {level}
+          </Checkbox>
+        )}
+    </div>
+
+    <div className="JobForm--section">
+      <label className="JobForm--sectionText">No. of working hours (per week)</label>
+      <div className="JobForm--hours">
+        <label>Min.</label>
+        <input type="text" value={''} />
+        <label>Max.</label>
+        <input type="text" value={''} />
+      </div>
+    </div>
+
+    <div className="text-right">
+      <Button className="Button--save" type="submit" onSubmit={onSubmit}>
+        Save
+      </Button>
+    </div>
+  </form>
 </div>
 
 export default JobFormComponent
