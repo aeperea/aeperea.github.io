9ce4ea7f52c97543745901645a16a948cb41767c
aeperea <elizondoperea@gmail.com>
2018-11-07 05:30:20 -0600

    Adding summary and changing styles

--- src/views/job-form/JobFormComponent.jsx
+++ src/views/job-form/JobFormComponent.jsx
@@ -5,65 +5,79 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import get from 'lodash/get'
 import includes from 'lodash/includes'
+import Input from '../../ui/input'
 import Icon from '../../ui/icon'
 import Button from '../../ui/button'
 import Checkbox from '../../ui/checkbox'
-
-const LEVELS = [
-  'Bachelor / Graduate',
-  'GCSE / A-Level / High School / GED',
-  'Master / Post-Graduate / PhD',
-  'Vocational / Diploma / Associates degree'
-];
+import {
+  UPDATE_EDUCATION, UPDATE_YEARS_OF_EXPERIENCE,
+  UPDATE_MIN_HOURS, UPDATE_MAX_HOURS
+} from '../../redux/constants'
+import {LEVELS, ONLY_DIGITS_REGEX} from '../../utils/generalUtils'
 
 const JobFormHeader = () => <div className="JobForm--header">
     <Icon type="number">1</Icon>
     <div className="JobForm--subheader">
-      <h2 className="JobForm--title">Job criteria</h2>
+      <h2 className="JobForm--title-form">Job criteria</h2>
       <div className="JobForm--description">We will use the data we collect here to better target your desired audicience.</div>
     </div>
   </div>
 
-export const onSubmit = () => {
+export const onSubmit = e => {
+  e.preventDefault()
   console.log("submitting")
 }
 
-export const JobFormComponent = ({isSummary, levelsOfEducation, yearsOfExperience, minHours, maxHours}) => {
-  console.log("JOB CRITERIA")
-  console.log(isSummary)
+export const JobFormComponent = ({isSummary, levelsOfEducation, yearsOfExperience,
+  minHours, maxHours, updateEducationList, updateYearsOfExperience,
+  updateMinHours, updateMaxHours}) => {
 
   return <div className="JobForm">
   <JobFormHeader />
   <form>
     <div className="JobForm--section">
       <label className="JobForm--sectionText">A minimum No. years of experience</label>
-      <input
-        placeholder="i.e. 5+"
-        type="text"
+      <Input
+        placeholder={"i.e. 5"}
+        type={"text"}
+        regex={ONLY_DIGITS_REGEX}
         value={yearsOfExperience}
+        onChange={e => updateYearsOfExperience(e.target.value)}
       />
     </div>
 
     <div className="JobForm--section">
       <label className="JobForm--sectionText">Level of education</label>
-      {LEVELS.map((level, index) =>
+      <div className="JobForm--optionGroup">
+        {LEVELS.map((level, index) =>
           <Checkbox
             key={`education-${index}`}
             name={level}
             checked={includes(levelsOfEducation, level)}
-            onChange={() => {console.log('checked')}}>
-          {level}
+            onChange={() => updateEducationList(level)}>
+            {level}
           </Checkbox>
         )}
+      </div>
     </div>
 
     <div className="JobForm--section">
       <label className="JobForm--sectionText">No. of working hours (per week)</label>
       <div className="JobForm--hours">
-        <label>Min.</label>
-        <input type="text" value={minHours} />
-        <label>Max.</label>
-        <input type="text" value={maxHours} />
+        <label className="JobForm--label">Min.</label>
+        <Input
+          placeholder={"0"}
+          type={"text"}
+          regex={ONLY_DIGITS_REGEX}
+          value={minHours}
+          onChange={e => updateMinHours(e.target.value)}/>
+        <label className="JobForm--label">Max.</label>
+        <Input
+          placeholder={"0"}
+          type={"text"}
+          regex={ONLY_DIGITS_REGEX}
+          value={maxHours}
+          onChange={e => updateMaxHours(e.target.value)}/>
       </div>
     </div>
 
@@ -77,14 +91,18 @@ export const JobFormComponent = ({isSummary, levelsOfEducation, yearsOfExperienc
 }
 
 JobFormComponent.propTypes = {
-  jobCriteria: PropTypes.obj,
+  jobCriteria: PropTypes.func,
   levelsOfEducation: PropTypes.array,
   yearsOfExperience: PropTypes.string,
   minHours: PropTypes.string,
-  maxHours: PropTypes.string
+  maxHours: PropTypes.string,
+  updateEducationList: PropTypes.func,
+  updateYearsOfExperience: PropTypes.func,
+  updateMinHours: PropTypes.func,
+  updateMaxHours: PropTypes.func,
 }
 
-export let mapStateToProps = ({jobCriteria}) => ({
+export const mapStateToProps = ({jobCriteria}) => ({
   isSummary: get(jobCriteria, 'isSummary', false),
   levelsOfEducation: get(jobCriteria, 'levelsOfEducation', []),
   yearsOfExperience: get(jobCriteria, 'yearsOfExperience', ''),
@@ -92,7 +110,12 @@ export let mapStateToProps = ({jobCriteria}) => ({
   maxHours: get(jobCriteria, 'maxHours', '')
 })
 
-export let mapDispatchToProps = null
+export const mapDispatchToProps = dispatch => ({
+  updateEducationList: level => dispatch({type: UPDATE_EDUCATION, level}),
+  updateYearsOfExperience: years => dispatch({type: UPDATE_YEARS_OF_EXPERIENCE, years}),
+  updateMinHours: min => dispatch({type: UPDATE_MIN_HOURS, min}),
+  updateMaxHours: max => dispatch({type: UPDATE_MAX_HOURS, max})
+})
 
 export default connect(
   mapStateToProps,
--- src/views/job-form/JobSummaryComponent.jsx
+++ src/views/job-form/JobSummaryComponent.jsx
@@ -1,9 +1,60 @@
 import './_JobForm.scss'
 
 import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import get from 'lodash/get'
+import Icon from '../../ui/icon'
+import Button from '../../ui/button'
 
-export const JobSummaryComponent = () => <div className="JobForm">
-  This is a filled Job Formed
+export const onSubmit = e => {
+  e.preventDefault()
+  console.log("submitting edit")
+}
+
+export const JobSummaryComponent = ({levelsOfEducation, yearsOfExperience, minMaxCopy}) => <div className="JobForm">
+  <div className="JobForm--header">
+    <h2 className="JobForm--title-summary"> <Icon type="success" />Job criteria</h2>
+    <div className="JobForm--subheader">
+      <Button className="Button--edit" onSubmit={onSubmit}>
+        Edit
+      </Button>
+    </div>
+  </div>
+  <div className="JobForm--section">
+    <div className="JobForm--summaryText">
+      A minimum No. years of experience: <span className="JobForm--summaryValue">{yearsOfExperience}</span>
+    </div>
+    <div className="JobForm--summaryText">
+      No. of working hours (per week): <span className="JobForm--summaryValue">{minMaxCopy}</span>
+    </div>
+  </div>
+  <div className="JobForm--section">
+    <div className="JobForm--sectionText">
+      Level of education
+    </div>
+    <div className="JobForm--tagList">
+      {levelsOfEducation.map((level, index) => <span key={`tags-${index}`} className="JobForm--tag">{level}</span>)}
+    </div>
+  </div>
 </div>
 
-export default JobSummaryComponent
+JobSummaryComponent.propTypes = {
+  jobCriteria: PropTypes.func,
+  levelsOfEducation: PropTypes.array,
+  yearsOfExperience: PropTypes.string,
+  minMaxCopy: PropTypes.string
+}
+
+export const mapStateToProps = ({jobCriteria}) => ({
+  levelsOfEducation: get(jobCriteria, 'levelsOfEducation', []),
+  yearsOfExperience: get(jobCriteria, 'yearsOfExperience', ''),
+  minMaxCopy: `${get(jobCriteria, 'minHours', '')}-${get(jobCriteria, 'maxHours', '')} hours`
+})
+
+export const mapDispatchToProps = null
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(JobSummaryComponent)
