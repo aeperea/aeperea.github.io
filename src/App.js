3ae3e5c2c20a2936b6a71db230c8742e51dd97cd
aeperea <elizondoperea@gmail.com>
2018-11-07 05:28:21 -0600

    Adding redux testing tools

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.scss'
 import React from 'react'
 import {createStore} from 'redux';
 import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension'
 import reducers from './redux/reducers';
 import {MainLayout} from './layout'
 
-const App = () => <Provider store={createStore(reducers)}>
+const store = createStore(
+  reducers,
+  composeWithDevTools()
+)
+
+const App = () => <Provider store={store}>
     <div className="App">
       <MainLayout />
     </div>
