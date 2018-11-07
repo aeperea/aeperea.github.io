902a6da75a11686932275497c853e14b72c0de1d
aeperea <elizondoperea@gmail.com>
2018-11-07 14:14:14 -0600

    Adding indent

--- src/views/job-summary/JobSummaryComponent.jsx
+++ src/views/job-summary/JobSummaryComponent.jsx
@@ -8,32 +8,33 @@ import Icon from '../../ui/icon'
 import Button from '../../ui/button'
 import {TOGGLE_SUMMARY_DISPLAY} from '../../redux/constants'
 
-export const JobSummaryComponent = ({levelsOfEducation, yearsOfExperience, minMaxCopy, toggleSummary}) => <div className="JobSummary">
-  <div className="JobSummary--header">
-    <h2 className="JobSummary--title"><Icon type="success"/>Job criteria</h2>
-    <div className="JobSummary--subheader">
-      <Button className="Button--edit" onClick={toggleSummary}>
-        Edit
-      </Button>
+export const JobSummaryComponent = ({levelsOfEducation, yearsOfExperience, minMaxCopy, toggleSummary}) =>
+  <div className="JobSummary">
+    <div className="JobSummary--header">
+      <h2 className="JobSummary--title"><Icon type="success"/>Job criteria</h2>
+      <div className="JobSummary--subheader">
+        <Button className="Button--edit" onClick={toggleSummary}>
+          Edit
+        </Button>
+      </div>
+    </div>
+    <div className="JobSummary--section">
+      <div className="JobSummary--summaryText">
+        A minimum No. years of experience: <span className="JobSummary--summaryValue">{yearsOfExperience}</span>
+      </div>
+      <div className="JobSummary--summaryText">
+        No. of working hours (per week): <span className="JobSummary--summaryValue">{minMaxCopy}</span>
+      </div>
+    </div>
+    <div className="JobSummary--section">
+      <div className="JobSummary--sectionText">
+        Level of education
+      </div>
+      <div className="JobSummary--tagList">
+        {levelsOfEducation && levelsOfEducation.map((level, index) => <span key={`tags-${index}`} className="JobSummary--tag">{level}</span>)}
+      </div>
     </div>
   </div>
-  <div className="JobSummary--section">
-    <div className="JobSummary--summaryText">
-      A minimum No. years of experience: <span className="JobSummary--summaryValue">{yearsOfExperience}</span>
-    </div>
-    <div className="JobSummary--summaryText">
-      No. of working hours (per week): <span className="JobSummary--summaryValue">{minMaxCopy}</span>
-    </div>
-  </div>
-  <div className="JobSummary--section">
-    <div className="JobSummary--sectionText">
-      Level of education
-    </div>
-    <div className="JobSummary--tagList">
-      {levelsOfEducation && levelsOfEducation.map((level, index) => <span key={`tags-${index}`} className="JobSummary--tag">{level}</span>)}
-    </div>
-  </div>
-</div>
 
 JobSummaryComponent.propTypes = {
   jobCriteria: PropTypes.func,
