b28fe80da2ab46f9eeda5b7487d071a427dbcaee
aeperea <elizondoperea@gmail.com>
2018-11-07 05:29:23 -0600

    Handling all utils separately

---
+++ src/utils/generalUtils.js
@@ -0,0 +1,8 @@
+export const LEVELS = [
+  'Bachelor / Graduate',
+  'GCSE / A-Level / High School / GED',
+  'Master / Post-Graduate / PhD',
+  'Vocational / Diploma / Associates degree'
+]
+
+export const ONLY_DIGITS_REGEX = /^\d+$/
