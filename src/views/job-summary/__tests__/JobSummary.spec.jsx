b31e57da2390d4bc870d7ddf8e02b81be0ceeb07
aeperea <elizondoperea@gmail.com>
2018-11-08 02:27:41 -0600

    Improving tests

--- src/views/job-summary/__tests__/JobSummary.spec.jsx
+++ src/views/job-summary/__tests__/JobSummary.spec.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import {shallow} from 'enzyme'
-import {JobSummaryComponent} from '../JobSummaryComponent'
+import {JobSummaryComponent, mapDispatchToProps} from '../JobSummaryComponent'
+import * as constants from '../../../redux/constants'
 
 describe('Job Summary', () => {
   it('should be a mountable component', () => {
     const wrapper = shallow(<JobSummaryComponent />)
     expect(wrapper.find('.JobSummary').length).toBeTruthy()
   })
-});
+
+  it('should render the same amount of elements as there is in the levels of education store value', () => {
+    const levelsOfEducation = ['asdf', 'asdfasdf', 'asdfasdfasdf']
+    const wrapper = shallow(<JobSummaryComponent levelsOfEducation={levelsOfEducation} />)
+    expect(wrapper.find('.JobSummary--tag').length).toBe(3)
+  })
+})
+
+describe('mapDispatchToProps', () => {
+  let dispatch = jest.fn()
+  let {toggleSummary} = mapDispatchToProps(dispatch)
+
+  it('should change the summary display flag', () => {
+    toggleSummary()
+    expect(dispatch).toBeCalledWith({type: constants.TOGGLE_SUMMARY_DISPLAY})
+  })
+})
